7766359228e8794a963b8178305cf4a0e458a1c7
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-03-12 20:36:50 +0530

    dashboard options

--- src/FnAComponent/DashboardOptions.jsx
+++ src/FnAComponent/DashboardOptions.jsx
@@ -7,9 +7,40 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+      background: "#FFCCBC",
+      boxShadow: "0px 4px 20px #525CDD",
+      borderRadius: "19px",
+      width: "200px",
+      height: "220px",
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+
+
 export default function DashboardOptions() {
+
+    const classes = useStyles();
+
     return (
-        <div>
+        <Grid container className={classes.root} spacing={2}>
+
+        <Grid item xs={12}>
+        <Grid container justify="center" spacing={2}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          ))}
+        </Grid>
             <img
                 src={bell}
                 alt="logo"
@@ -26,6 +57,7 @@ export default function DashboardOptions() {
                 src={fee}
                 alt="logo"
             />
-        </div>
+      </Grid>
+      </Grid>
     )
 }
