1b887f80e83c7bbf7a218b7673c2e3906b40d621
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-03-15 16:36:38 +0530

    routing fix

--- src/FnAComponent/DashboardOptions.jsx
+++ src/FnAComponent/DashboardOptions.jsx
@@ -1,262 +1,240 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import bell from "../images/Bell.svg";
 import deadline from "../images/Deadline.svg";
 import list from "../images/DefaulterList.svg";
 import fee from "../images/Fee.svg";
-import { makeStyles,withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Dialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
-import Grow from '@material-ui/core/Grow';
-import FnADesign from './FnADashboard.module.css';
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import Grow from "@material-ui/core/Grow";
+import FnADesign from "./FnADashboard.module.css";
 import { useHistory } from "react-router-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    background: "#FFCCBC",
+    boxShadow: "0px 4px 20px #525CDD",
+    borderRadius: "19px",
+    width: "200px",
+    height: "220px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#D3E3FC",
+      boxShadow: "none",
     },
-    paper: {
-      background: "#FFCCBC",
-      boxShadow: "0px 4px 20px #525CDD",
-      borderRadius: "19px",
-      width: "200px",
-      height: "220px",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      cursor: "pointer",
-      "&:hover": {
-        backgroundColor: '#D3E3FC',
-        boxShadow: 'none',
-      }
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }));
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-  const styles = (theme) => ({
-    root: {
-      margin: 0,
-      padding: theme.spacing(2),
-    },
-    closeButton: {
-      position: 'absolute',
-      right: theme.spacing(1),
-      top: theme.spacing(1),
-      color: theme.palette.grey[500],
-    },
-    paper: {
-      borderRadius: 20,
-    }
-  });
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  paper: {
+    borderRadius: 20,
+  },
+});
 
 const DialogTitle = withStyles(styles)((props) => {
-    const { children, classes, onClose, ...other } = props;
-    return (
-      <MuiDialogTitle disableTypography className={classes.root} {...other}>
-        <Typography variant="h6">{children}</Typography>
-        {onClose ? (
-          <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </MuiDialogTitle>
-    );
-  });
-  
-  const NotifyDialog = withStyles((theme) => ({
-    paper: {
-      borderRadius: 20,
-    },
-  }))(Dialog);
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
 
+const NotifyDialog = withStyles((theme) => ({
+  paper: {
+    borderRadius: 20,
+  },
+}))(Dialog);
 
-  const DialogContent = withStyles((theme) => ({
-    root: {
-      padding: theme.spacing(2),
-    },
-  }))(MuiDialogContent);
-  
-  const DialogActions = withStyles((theme) => ({
-    root: {
-      margin: 0,
-      padding: theme.spacing(1),
-    },
-  }))(MuiDialogActions);
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
 
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 //Alert on sending notifications
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-
 export default function DashboardOptions() {
+  const classes = useStyles();
+  const history = useHistory();
 
-    const classes = useStyles();
-    const history = useHistory();
+  //Glow Effect
+  const [checked, setChecked] = React.useState(false);
 
-    //Glow Effect
-    const [checked, setChecked] = React.useState(false);
+  const handleChangeEffect = () => {
+    setChecked((prev) => !prev);
+  };
 
-    const handleChangeEffect = () => {
-      setChecked((prev) => !prev);
-    };
+  useEffect(() => {
+    handleChangeEffect();
+  }, []);
 
-    useEffect(() => {
-      handleChangeEffect();
-    },[]);
+  // Notification Dialog
+  const [open, setOpen] = React.useState(false);
 
-    // Notification Dialog
-    const [open, setOpen] = React.useState(false);
-  
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-    const handleClose = () => {
-      setOpen(false);
-    };
-    
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    //Alert 
-    const [openAlert, setOpenAlert] = React.useState(false);
+  //Alert
+  const [openAlert, setOpenAlert] = React.useState(false);
 
-    const handleClickAlert = () => {
-      setOpenAlert(true);
-    };
+  const handleClickAlert = () => {
+    setOpenAlert(true);
+  };
 
-    const handleCloseAlert = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
+  const handleCloseAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-      setOpenAlert(false);
-    };
+    setOpenAlert(false);
+  };
 
-    const handleSend = () => {
-      setOpen(false);
-      setOpenAlert(true);
-    };
+  const handleSend = () => {
+    setOpen(false);
+    setOpenAlert(true);
+  };
 
-  
-    return (
-      <div>
+  return (
+    <div>
+      <NotifyDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Notify Students
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Dear Navneet Bhole, you are requested to pay your due fees.Please
+            ignore if already paid.
+          </Typography>
+          <Typography gutterBottom>
+            Dear Navneet Bhole, you are requested to pay your due fees.Please
+            ignore if already paid.
+          </Typography>
+          <Typography gutterBottom>
+            Dear Navneet Bhole, you are requested to pay your due fees.Please
+            ignore if already paid.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleSend} color="primary">
+            SEND
+          </Button>
+        </DialogActions>
+      </NotifyDialog>
 
-        <NotifyDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-                <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-                  Notify Students
-                </DialogTitle>
-                <DialogContent dividers>
-                  <Typography gutterBottom>
-                    Dear Navneet Bhole, you are requested to pay
-                    your due fees.Please ignore if already paid.
-                  </Typography>
-                  <Typography gutterBottom>
-                    Dear Navneet Bhole, you are requested to pay
-                    your due fees.Please ignore if already paid.
-                  </Typography>
-                  <Typography gutterBottom>
-                    Dear Navneet Bhole, you are requested to pay
-                    your due fees.Please ignore if already paid.
-                  </Typography>
-                </DialogContent>
-                <DialogActions>
-                  <Button autoFocus onClick={handleSend} color="primary">
-                    SEND
-                  </Button>
-                </DialogActions>
-          </NotifyDialog>
+      {/* Alert */}
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+      >
+        <Alert onClose={handleCloseAlert} severity="success">
+          Notifications sent successfully!
+        </Alert>
+      </Snackbar>
 
-          {/* Alert */}
-          <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
-            <Alert onClose={handleCloseAlert} severity="success">
-              Notifications sent successfully!
-            </Alert>
-          </Snackbar>
-
-          <Grid container className={classes.root} spacing={2}>
-          <Grid item xs={12}>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
           <Grid container justify="center" spacing={5}>
             <Grow in={checked}>
               <Grid item>
-              <Link to="/feechart">
+                <Link to="/FnA/feechart">
                   <Paper className={classes.paper}>
-                    <img
-                    src={fee}
-                    alt="logo"
-                    />
+                    <img src={fee} alt="logo" />
                     <div className={FnADesign.paperTxt}>Fee Chart</div>
                   </Paper>
                 </Link>
               </Grid>
             </Grow>
-            <Grow
-                in={checked}
-                {...(checked ? { timeout: 500 } : {})}
-              >
+            <Grow in={checked} {...(checked ? { timeout: 500 } : {})}>
               <Grid item>
-              <Link to="/DefaulterList">
-                <Paper className={classes.paper}>
-                <img
-                  src={list}
-                  alt="logo"
-                  />
-                  <div className={FnADesign.paperTxt}>Defaulters List</div>
-                </Paper>
+                <Link to="/FnA/DefaulterList">
+                  <Paper className={classes.paper}>
+                    <img src={list} alt="logo" />
+                    <div className={FnADesign.paperTxt}>Defaulters List</div>
+                  </Paper>
                 </Link>
               </Grid>
-              </Grow>
-              <Grow
-                in={checked}
-                {...(checked ? { timeout: 500 } : {})}
-              >
-              <Grid item  onClick={handleClickOpen}>
+            </Grow>
+            <Grow in={checked} {...(checked ? { timeout: 500 } : {})}>
+              <Grid item onClick={handleClickOpen}>
                 <Paper className={classes.paper}>
-                  <img
-                      src={bell}
-                      alt="logo"
-                  />
+                  <img src={bell} alt="logo" />
                   <div className={FnADesign.paperTxt}>Send Notifications</div>
                 </Paper>
               </Grid>
-              </Grow>
-              <Grow
-                in={checked}
-                {...(checked ? { timeout: 500 } : {})}
-              >
+            </Grow>
+            <Grow in={checked} {...(checked ? { timeout: 500 } : {})}>
               <Grid item>
-              <Link to="/deadlineext">
-                <Paper className={classes.paper}>
-                <img
-                  src={deadline}
-                  alt="logo"
-                  />
-                  <div className={FnADesign.paperTxt}>Deadline Extension</div>
-                </Paper>
+                <Link to="/FnA/deadlineext">
+                  <Paper className={classes.paper}>
+                    <img src={deadline} alt="logo" />
+                    <div className={FnADesign.paperTxt}>Deadline Extension</div>
+                  </Paper>
                 </Link>
               </Grid>
-              </Grow>
+            </Grow>
           </Grid>
-          </Grid>
-          </Grid>
-      </div>
-    )
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
--- src/FnAComponent/FnADashboard.jsx
+++ src/FnAComponent/FnADashboard.jsx
@@ -1,50 +1,50 @@
-import React from 'react';
-import FnADesign from './FnADashboard.module.css';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import FnADesign from "./FnADashboard.module.css";
+import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
-import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import DashboardOptions from './DashboardOptions';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import DashboardOptions from "./DashboardOptions";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const LogoutBtn = withStyles(() => ({
-    root: {
-      width: "180px",
-      height: "45px",
-      color: "#000000",
-      background: "#FFCCBC",
-      boxShadow: "0px 4px 20px #525CDD",
-      borderRadius: "10px",
-      letterSpacing: "1.25px",
-      '&:hover': {
-        backgroundColor: "#070D59",
-        color: '#ffffff',
-        boxShadow: 'none'
-      },
+  root: {
+    width: "180px",
+    height: "45px",
+    color: "#000000",
+    background: "#FFCCBC",
+    boxShadow: "0px 4px 20px #525CDD",
+    borderRadius: "10px",
+    letterSpacing: "1.25px",
+    "&:hover": {
+      backgroundColor: "#070D59",
+      color: "#ffffff",
+      boxShadow: "none",
     },
-  }))(Button);
+  },
+}))(Button);
 
 export default function FnADashboard() {
-    const history = useHistory();
-    const goLogout = () => history.push('login');
+  const history = useHistory();
+  const goLogout = () => history.push("login");
 
-    return (
-        <div className={FnADesign.main}>
-            <div className={FnADesign.Nav}>
-                <div className={FnADesign.HeadingTxt}>
-                    WELCOME TO FEE AND ADMINISTRATION
-                </div>
-                <LogoutBtn variant="contained" color="primary" onClick={goLogout} >
-                    Logout
-                </LogoutBtn>
-            </div>
-            <div className={FnADesign.DashboardFeat}>
-                <DashboardOptions />
-            </div>
+  return (
+    <div className={FnADesign.main}>
+      <div className={FnADesign.Nav}>
+        <div className={FnADesign.HeadingTxt}>
+          WELCOME TO FEE AND ADMINISTRATION
         </div>
-    )
+        <LogoutBtn variant="contained" color="primary" onClick={goLogout}>
+          Logout
+        </LogoutBtn>
+      </div>
+      <div className={FnADesign.DashboardFeat}>
+        <DashboardOptions />
+      </div>
+    </div>
+  );
 }
