059d5361d9c414854a3572990294e52063996144
Ayush <ayushkhandelwal3217@gmail.com>
2021-03-16 19:15:45 +0530

    menu for student header

--- src/StudentComponents/HeaderComponents/HeaderNav.jsx
+++ src/StudentComponents/HeaderComponents/HeaderNav.jsx
@@ -1,13 +1,37 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, Button, Menu, MenuItem } from '@material-ui/core'
 import header from "./Header.module.css"
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
 
 
 function HeaderNav() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     return (
         <div className={header.nav}>
             {/* <Notification/> */}
-            <Avatar/>
+            <div>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                    <Avatar />
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem><Link to='/'>Logout</Link></MenuItem>
+                </Menu>
+            </div>
         </div>
     )
 }
