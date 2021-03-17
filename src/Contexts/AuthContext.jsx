7a4f68f8b783a8cc4d68fbc193c242e512026818
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-03-17 16:37:14 +0530

    wip

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,18 +1,1 @@
-import React, { useContext, useState, useEffect } from "react";
-import { auth } from "../Firebase";
-
-const AuthContext = React.createContext();
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
-
-export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState();
-
-  const value = {
-    currentUser,
-  };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
+import React, { useContext, createContext, useState, useEffect } from "react";
