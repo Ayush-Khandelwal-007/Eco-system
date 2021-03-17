fef7a3cb6f3a5240085b1de5899f85419fd69730
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-03-17 16:47:58 +0530

    working on auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useMemo } from "react";
 import "./App.css";
 import {
   HashRouter as Router,
@@ -10,31 +11,36 @@ import Login from "./pages/Login";
 import Landing from "./pages/Landing";
 import FnA from "./pages/FnA";
 import Hod from "./pages/Hod";
-import { auth, db } from "./Firebase";
+import { AuthContext } from "./contexts/AuthContext";
 
 function App() {
+  const [user, setUser] = useState();
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
     <div className="App">
       <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/studentDashboard">
-              <StudentDashboard />
-            </Route>
-            <Route path="/HODDashboard">
-              <Hod />
-            </Route>
-            <Route path="/FnADashBoard">
-              <FnA />
-            </Route>
-            <Route path="/Login">
-              <Login />
-            </Route>
-            <Route exact path="/">
-              <Landing />
-            </Route>
-          </Switch>
-        </div>
+        <AuthContext.Provider value={providerValue}>
+          <div className="App">
+            <Switch>
+              <Route path="/studentDashboard">
+                <StudentDashboard />
+              </Route>
+              <Route path="/HODDashboard">
+                <Hod />
+              </Route>
+              <Route path="/FnADashBoard">
+                <FnA />
+              </Route>
+              <Route path="/Login">
+                <Login />
+              </Route>
+              <Route exact path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </div>
+        </AuthContext.Provider>
       </Router>
     </div>
   );
--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,1 +1,3 @@
 import React, { useContext, createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext(null);
