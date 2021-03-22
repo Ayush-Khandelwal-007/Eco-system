971f91cb44b6871c491d15a135b968ca66820b45
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-03-22 18:26:09 +0530

    faculty list

--- src/HoDComponent/FacultyList.jsx
+++ src/HoDComponent/FacultyList.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import HoDNav from "./HoDNav";
+import { useHistory } from "react-router-dom";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Container from "@material-ui/core/Container";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Zoom from "@material-ui/core/Zoom";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import design from "./hod.module.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  grow: {
+    flexGrow: 1,
+    background: "#77A6F7",
+  },
+
+  appBar: {
+    background: "#070D59",
+  },
+
+  paper: {},
+  GridContainer: {
+    background: "#FFCCBC",
+    cursor: "pointer",
+    height: "180px",
+    "&:hover": {
+      backgroundColor: "#D3E3FC",
+      boxShadow: "none",
+    },
+  },
+}));
 
 function FacultyList() {
-  return <div></div>;
+  const history = useHistory();
+  const classes = useStyles();
+
+  return (
+    <div className={classes.grow}>
+      <CssBaseline />
+      <AppBar className={classes.appBar}>
+        <HoDNav />
+      </AppBar>
+    </div>
+  );
 }
 
 export default FacultyList;
