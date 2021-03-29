27505719316917c1a4d93f0d6adeae28a12d1e0b
Ayush <ayushkhandelwal3217@gmail.com>
2021-03-29 16:12:39 +0530

    fee payment student side done

--- src/StudentComponents/FeesPortal.jsx
+++ src/StudentComponents/FeesPortal.jsx
@@ -1,92 +1,113 @@
 import { Button } from '@material-ui/core'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useUser } from '../contexts/User';
 import { db } from '../Firebase';
 import feesPortal from './FeesPortalComponent/FeesPortal.module.css'
 
 function FeesPortal() {
 
-    const path =(loginType)=>{
+    const path = (loginType) => {
         var userType;
         switch (loginType) {
             case 1:
-              userType = "Students";
-              break;
+                userType = "Students";
+                break;
             case 2:
-              userType = "FnA";
-              break;
+                userType = "FnA";
+                break;
             case 3:
-              userType = "HoD";
-              break;
+                userType = "HoD";
+                break;
             default:
-              return null;
-          }
-          return userType
+                return null;
+        }
+        return userType
     }
 
     const [state, dispatch] = useUser();
-    const fee=state?.user?.fees;
-    const payFees=()=>{
-        dispatch({
-            type:"PAY_FEE",
-        })
-        // db.collection("feeapproval").doc(state.user.email).set({
-        //     description:JSON.stringify(state.user.fees)
-        // })
-        var a=path(state.userType);
-        var email=state.user.email;
-        console.log(email);
-        db.collection("users").doc(a).collection(email).doc(email).set({
-            ...state.user
+    const fee = state?.user?.fees;
+    const payFees = async () => {
+        var userType = path(parseInt(state.userType));
+        var email = state.user.email;
+        db.collection("users").doc(userType).collection(email).doc(email).update({
+            fees: {
+                latefee: state.user.fees.latefee,
+                semfee: state.user.fees.semfee,
+                paid: state.user.fees.semfee + state.user.fees.latefee,
+                due: 0,
+            },
         });
-        // localStorage.setItem('user', JSON.stringify(state.user));
+
+        try{
+            var docRef = db.collection("users").doc(userType).collection(email);
+        var alldata = await docRef.get();
+        alldata.forEach((doc) => {
+            if (doc.data()) {
+                dispatch({
+                  type: "SET_USER",
+                  user: { ...doc.data() },
+                  userType: state.userType,
+                });
+                localStorage.setItem('user', JSON.stringify(doc.data()));
+
+            } else {
+              console.log(doc.data());
+            }
+          });
+        } catch(error){
+            console.log(error);
+        }
+        localStorage.setItem('user', JSON.stringify(state.user));
+        db.collection("feeapproval").doc(state.user.email).set({
+            description: JSON.stringify(state.user.fees)
+        })
     }
     return (
-        state.user &&(
-        <div className={feesPortal.screen}>
-            <div className={feesPortal.infoDiv} >
-                <h1>FEE STATUS</h1>
-                {
-                    state.user.feesApproved===true?(
-                        <div>
-                            Fees Paid And Approved
-                        </div>
-                    ):(
-                        <div className={feesPortal.infoGrid}>
-                    <div>
-                        <div>SEMESTER FEE :</div>
-                        <div>{fee?.semfee}</div>
-                    </div>
-                    <div>
-                        <div>LATE FEE :</div>
-                        <div>{fee?.latefee}</div>
-                    </div>
-                    <div>
-                        <div>PAID FEES :</div>
-                        <div>{fee?.paid}</div>
-                    </div>
-                    <div>
-                        <div>DUE FEE :</div>
-                        <div>{fee?.due}</div>
-                    </div>
-                    <div>
-                        <div>DEADLINE :</div>
-                        <div>20-03-2021</div>
-                    </div>
+        state.user && (
+            <div className={feesPortal.screen}>
+                <div className={feesPortal.infoDiv} >
+                    <h1>FEE STATUS</h1>
+                    {
+                        state.user.feesApproved === true ? (
+                            <div>
+                                Fees Paid And Approved
+                            </div>
+                        ) : (
+                            <div className={feesPortal.infoGrid}>
+                                <div>
+                                    <div>SEMESTER FEE :</div>
+                                    <div>{fee?.semfee}</div>
+                                </div>
+                                <div>
+                                    <div>LATE FEE :</div>
+                                    <div>{fee?.latefee}</div>
+                                </div>
+                                <div>
+                                    <div>PAID FEES :</div>
+                                    <div>{fee?.paid}</div>
+                                </div>
+                                <div>
+                                    <div>DUE FEE :</div>
+                                    <div>{fee?.due}</div>
+                                </div>
+                                <div>
+                                    <div>DEADLINE :</div>
+                                    <div>20-03-2021</div>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-                    )
-                }
-            </div>
-            {
-                state.user.feesApproved===true?(null):(
-                        fee.semfee+fee.latefee===fee.paid ? (
+                {
+                    state.user.feesApproved === true ? (null) : (
+                        fee.semfee + fee.latefee === fee.paid ? (
                             <div className={feesPortal.ApprovalDiv}>Fees Submitted. Under Approval Process</div>
                         ) : (
-                            <div className={feesPortal.buttonDiv}><Button onClick={()=>payFees()}>Pay Fees</Button></div>
+                            <div className={feesPortal.buttonDiv}><Button onClick={() => payFees()}>Pay Fees</Button></div>
                         )
-                )
-            }
-        </div>)
+                    )
+                }
+            </div>)
     )
 }
 
