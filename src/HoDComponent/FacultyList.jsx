1297b6ac6fc6fbd92cc1a538b21f05b27f96b8c9
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-06 14:21:22 +0530

    fculty list

--- src/HoDComponent/FacultyList.jsx
+++ src/HoDComponent/FacultyList.jsx
@@ -8,6 +8,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -25,9 +26,54 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+  container: {
+    width: "auto",
+    margin: 25,
+  },
+});
+
 function FacultyList() {
   const history = useHistory();
+  const classes = useStyles();
 
-  return <> Hi</>;
+  return (
+    <TableContainer component={Paper} className={classes.container}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>#</StyledTableCell>
+            <StyledTableCell>Enroll&nbsp;No.</StyledTableCell>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell>Semester</StyledTableCell>
+            <StyledTableCell align="right">
+              Outstanding&nbsp;FEE
+            </StyledTableCell>
+            <StyledTableCell align="right">DEADLINE</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <StyledTableRow key={index}>
+              <StyledTableCell component="th" scope="row">
+                {index + 1}
+              </StyledTableCell>
+              <StyledTableCell>{row.Enrollno}</StyledTableCell>
+              <StyledTableCell>{row.Name}</StyledTableCell>
+              <StyledTableCell>{row.Batch}</StyledTableCell>
+              <StyledTableCell align="right">
+                {row.OutstandingFee}
+                {row.OutstandingFee === 0 ? "(Paid now)" : null}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.Deadline}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 export default FacultyList;
