e5c87f0cbcfda1261007df9c78340304b6808601
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-06 14:06:01 +0530

    faculty list hod

--- src/HoDComponent/FacultyList.jsx
+++ src/HoDComponent/FacultyList.jsx
@@ -1,39 +1,32 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import design from "./hod.module.css";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
-const useStyles = makeStyles((theme) => ({
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "#FFCCBC",
+    color: "#000000",
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
   root: {
-    position: "fixed",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
+    backgroundColor: "#fffff",
   },
-  grow: {
-    flexGrow: 1,
-    background: "#77A6F7",
-  },
-
-  appBar: {
-    background: "#070D59",
-  },
-
-  paper: {},
-  GridContainer: {
-    background: "#FFCCBC",
-    cursor: "pointer",
-    height: "180px",
-    "&:hover": {
-      backgroundColor: "#D3E3FC",
-      boxShadow: "none",
-    },
-  },
-}));
+}))(TableRow);
 
 function FacultyList() {
   const history = useHistory();
-  const classes = useStyles();
 
   return <> Hi</>;
 }
