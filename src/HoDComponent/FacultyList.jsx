5e9b79dd1073d128b8013665a86e38ea66355ed1
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-06 16:24:47 +0530

    faculty list

--- src/HoDComponent/FacultyList.jsx
+++ src/HoDComponent/FacultyList.jsx
@@ -10,6 +10,13 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Faculty from "./FacultyListComponent/Faculty.module.css";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -44,25 +51,130 @@ function createData(Name) {
 const rows1 = [
   createData("Triloki Pant"),
   createData("Anand Kumar Tiwari"),
-  createData("Triloki Pant"),
-  createData("Anand Kumar Tiwari"),
-  createData("Triloki Pant"),
+  createData("Anjali Gautam"),
 ];
 
 const rows2 = [
-  createData("Anand Kumar Tiwari"),
-  createData("Triloki Pant"),
-  createData("Anand Kumar Tiwari"),
-  createData("Triloki Pant"),
-  createData("Anand Kumar Tiwari"),
+  createData("Praveen Kumar"),
+  createData("Sumita Das"),
+  createData("Jaspreet Singh"),
 ];
+
+//dailog
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 function FacultyList() {
   const history = useHistory();
   const classes = useStyles();
 
+  //dailog
+  const [openAdd, setOpenAdd] = React.useState(false);
+
+  const handleClickOpenAdd = () => {
+    setOpenAdd(true);
+  };
+  const handleCloseAdd = () => {
+    setOpenAdd(false);
+  };
+
+  const [openDelete, setopenDelete] = React.useState(false);
+
+  const handleClickOpenDelete = () => {
+    setopenDelete(true);
+  };
+  const handleCloseDelete = () => {
+    setopenDelete(false);
+  };
+
   return (
     <div className={Faculty.main}>
+      {/* dailog */}
+      <Dialog
+        onClose={handleCloseAdd}
+        aria-labelledby="customized-dialog-title"
+        open={openAdd}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleCloseAdd}>
+          ADD
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+            ac consectetur ac, vestibulum at eros.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCloseAdd} color="primary">
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* Delete Dailog */}
+      <Dialog
+        onClose={handleCloseDelete}
+        aria-labelledby="customized-dialog-title"
+        open={openDelete}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleCloseDelete}>
+          Confirm Delete
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>Are you sure you want to delete?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCloseDelete} color="primary">
+            No
+          </Button>
+          <Button autoFocus onClick={handleCloseDelete} color="primary">
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* dialog */}
       <TableContainer component={Paper} className={classes.container}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
@@ -80,7 +192,13 @@ function FacultyList() {
                 </StyledTableCell>
                 <StyledTableCell>{row.Name}</StyledTableCell>
                 <StyledTableCell align="right">
-                  <button>DELETE</button>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={handleClickOpenDelete}
+                  >
+                    Delete
+                  </Button>
                 </StyledTableCell>
               </StyledTableRow>
             ))}
@@ -104,7 +222,13 @@ function FacultyList() {
                 </StyledTableCell>
                 <StyledTableCell>{row.Name}</StyledTableCell>
                 <StyledTableCell align="right">
-                  <button>ADD</button>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={handleClickOpenAdd}
+                  >
+                    ADD
+                  </Button>
                 </StyledTableCell>
               </StyledTableRow>
             ))}
