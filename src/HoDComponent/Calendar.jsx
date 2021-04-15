80a3616d5f307da9cd6313379b14529738faad12
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-15 13:23:18 +0530

    calender color

--- src/HoDComponent/Calendar.jsx
+++ src/HoDComponent/Calendar.jsx
@@ -1,59 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { db } from '../Firebase'
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { db } from "../Firebase";
 
 function Calendar() {
-
   const [events, setevents] = useState([]);
 
-    useEffect(() => {
-        db.collection('calendar').onSnapshot(snapshot => {
-          setevents(snapshot.docs.map((doc) => doc.data()))
-          //{ title: 'event 1', start: '2021-03-17' ,end: '2021-03-19' , display: 'block'} format of event
-        })
-      }, [db])
-    
-    return (
-        <div style={{
-            width: '100%',
-            background: "#835712;",
-            color: "white",
-        }}>
-            <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                headerToolbar={{
-                    right: 'prev,next today',
-                    left: 'title',
-                    center: 'dayGridMonth,timeGridWeek,timeGridDay'
-                }}
-                events={events}
-                eventDisplay='block'
-                height={500}
-                selectable={true}
-                dateClick={(info) => {
-                    console.log(info);
-                }}
-            // weekends={this.state.weekendsVisible}
-            // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            // select={this.handleDateSelect}
-            // eventContent={renderEventContent} // custom render function
-            // eventClick={function(info) {
-            //   info.jsEvent.preventDefault(); // don't let the browser navigate
-            //   console.log(info.event)
-            // }}
-            // eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
+  useEffect(() => {
+    db.collection("calendar").onSnapshot((snapshot) => {
+      setevents(snapshot.docs.map((doc) => doc.data()));
+      //{ title: 'event 1', start: '2021-03-17' ,end: '2021-03-19' , display: 'block'} format of event
+    });
+  }, [db]);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        background: "#835712",
+        color: "white",
+      }}
+    >
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          right: "prev,next today",
+          left: "title",
+          center: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        events={events}
+        eventDisplay="block"
+        height={500}
+        selectable={true}
+        dateClick={(info) => {
+          console.log(info);
+        }}
+        // weekends={this.state.weekendsVisible}
+        // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        // select={this.handleDateSelect}
+        // eventContent={renderEventContent} // custom render function
+        // eventClick={function(info) {
+        //   info.jsEvent.preventDefault(); // don't let the browser navigate
+        //   console.log(info.event)
+        // }}
+        // eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+        /* you can update a remote database when these fire:
             eventAdd={function(){}}
             eventChange={function(){}}
             eventRemove={function(){}}
             */
-            />
-        </div>
-    )
+      />
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
