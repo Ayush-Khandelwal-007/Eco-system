9e9469b4ffc63b444953e6c4bdb644fb6e310933
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-15 12:51:50 +0530

    profile List

--- src/HoDComponent/ProfileView.jsx
+++ src/HoDComponent/ProfileView.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import design from "./ProfileViewComponent/ProfileView.module.css";
 
@@ -9,6 +15,10 @@ const useStyles = makeStyles((theme) => ({
     position: "fixed",
     bottom: theme.spacing(2),
     right: theme.spacing(2),
+  },
+  Listroot: {
+    width: "100%",
+    maxWidth: "36ch",
   },
   grow: {
     flexGrow: 1,
@@ -29,6 +39,9 @@ const useStyles = makeStyles((theme) => ({
       boxShadow: "none",
     },
   },
+  inline: {
+    display: "inline",
+  },
 }));
 
 function ProfileView() {
@@ -40,6 +53,70 @@ function ProfileView() {
       <div className={design.heading}>
         <h1>Profile View</h1>
       </div>
+      <List className={classes.root}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Nischay Nagar" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="IIT2019198"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Nischay Nagar
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Ayush Khandu" src="/static/images/avatar/2.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="IIT2019240"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Ayush Khandelwal
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Rahul Rai" src="/static/images/avatar/3.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="IIT2019194"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Rahul Rai
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
     </div>
   );
 }
