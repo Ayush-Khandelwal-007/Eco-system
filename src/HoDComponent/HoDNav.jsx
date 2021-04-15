a06c656719bd8b369be7caa975b99005dfda7aa0
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-15 15:10:19 +0530

    drawer color

--- src/HoDComponent/HoDNav.jsx
+++ src/HoDComponent/HoDNav.jsx
@@ -107,6 +107,10 @@ const useStyles = makeStyles((theme) => ({
   chevicon: {
     marginLeft: "auto",
   },
+  NotificationLi: {
+    background: "#fff",
+    padding: "0 10px",
+  },
 }));
 
 function HoDNav() {
@@ -250,13 +254,13 @@ function HoDNav() {
           </IconButton>
         </div>
         {notifications.map((not, index) => {
-              return (
-                <div key={not.id}>
-                  <Typography gutterBottom>{not.message}</Typography>
-                  {index === notifications.length - 1 ? null : <hr />}
-                </div>
-              );
-            })}
+          return (
+            <div key={not.id} className={classes.NotificationLi}>
+              <Typography gutterBottom>{not.message}</Typography>
+              {index === notifications.length - 1 ? null : <hr />}
+            </div>
+          );
+        })}
         <Divider />
       </Drawer>
       <Toolbar>
