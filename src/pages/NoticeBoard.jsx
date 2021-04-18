7f7305ddcd2bfcd87ad6a64e9d028e03c7ffedcf
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-18 17:38:04 +0530

    react crousel

--- src/pages/NoticeBoard.jsx
+++ src/pages/NoticeBoard.jsx
@@ -1,29 +1,76 @@
 import React from "react";
-import Carousel from "react-animated-carousel";
-import "../StudentComponents/NoticeBoardComponent/NoticeBoard.css";
+import design from "../StudentComponents/NoticeBoardComponent/NoticeBoard.module.css";
+import { logo } from "../images";
+import Carousel from "react-material-ui-carousel";
+import { useHistory } from "react-router-dom";
+import { Paper, Button } from "@material-ui/core";
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 
-let slides = [<h1>asdasd 1</h1>, <h1>qwerty 2</h1>, <h1>zxcv 3</h1>];
+var items = [
+  {
+    name: "Notice #1",
+    description: "Probably the most random thing you have ever seen!",
+  },
+  {
+    name: "Notice #2",
+    description: "Hello World!",
+  },
+];
+
+const LoginBtn = withStyles(() => ({
+  root: {
+    width: "20vw",
+    minWidth: "200px",
+    maxWidth: "250px",
+    height: "50px",
+    color: "#fff",
+    borderRadius: "10px",
+    backgroundColor: "rgba(7, 13, 89, 0.9)",
+    letterSpacing: "1.25px",
+    "&:hover": {
+      backgroundColor: "#070D59",
+    },
+  },
+}))(Button);
 
 function NoticeBoard() {
+  const history = useHistory();
+  const goLogin = () => history.push("login");
+
   return (
-    <div className="NoticeCrousel">
-      <Carousel
-        slides={slides}
-        animationType="FADE"
-        animationDuration={1000}
-        duration={2000}
-        animationTimingFunction="linear"
-        withNavigation
-      />
-      <Carousel
-        slides={slides}
-        animationType="SLIDE"
-        duration={2000}
-        withNavigation
-      />
-      <Carousel slides={slides} animationType="ZOOM" duration={3000} />
-      <Carousel slides={slides} duration={3500} withNavigation />
+    <div className={design.main}>
+      <img src={logo} alt="" />
+      <Carousel>
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+      <LoginBtn
+        style={{ background: "#27AE60" }}
+        variant="contained"
+        color="primary"
+        onClick={goLogin}
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ delay: "0.2", type: "spring", stiffness: 50 }}
+      >
+        Login
+      </LoginBtn>
     </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+    </Paper>
   );
 }
 
