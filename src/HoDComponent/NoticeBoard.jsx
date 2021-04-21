96bf852016b6070a7396910ba3642add0c1a20d7
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-04-21 18:50:25 +0530

    noticeboard

--- src/HoDComponent/NoticeBoard.jsx
+++ src/HoDComponent/NoticeBoard.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import design from "./NoticeBoardComponent/NoticeBoard.module.css";
+import { db } from "../Firebase";
 
 function NoticeBoard() {
-  return <div></div>;
+  return (
+    <div className={design.main}>
+      <div className={design.heading}>
+        <h1>Profile View</h1>
+      </div>
+    </div>
+  );
 }
 
 export default NoticeBoard;
