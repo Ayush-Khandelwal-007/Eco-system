8ad96f3e37f075fe75c3931ee4021d061e549c09
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-05 16:54:20 +0530

    student register

--- src/FnAComponent/RegisterNewStudent/CoursesForm.jsx
+++ src/FnAComponent/RegisterNewStudent/CoursesForm.jsx
@@ -4,13 +4,59 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    width: "100%",
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function CoursesForm() {
+  const classes = useStyles();
+  const [branch, setBranch] = React.useState("");
+
+  const handleChange = (event) => {
+    setBranch(event.target.value);
+  };
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Courses Details
       </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">
+              Branch
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={branch}
+              onChange={handleChange}
+              label="Branch"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"IT"}>IT</MenuItem>
+              <MenuItem value={"ECE"}>ECE</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
