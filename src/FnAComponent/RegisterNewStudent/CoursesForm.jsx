136adff05c351af4f7ea3f89434b7b20a56e7c9d
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-05 17:16:38 +0530

    student register

--- src/FnAComponent/RegisterNewStudent/CoursesForm.jsx
+++ src/FnAComponent/RegisterNewStudent/CoursesForm.jsx
@@ -10,6 +10,13 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import CommentIcon from "@material-ui/icons/Comment";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -28,6 +35,54 @@ export default function CoursesForm() {
 
   const handleChange = (event) => {
     setBranch(event.target.value);
+  };
+
+  // Courses list
+  //Core
+  const [checkedCore, setCheckedCore] = React.useState([0]);
+
+  const handleToggleCore = (value) => () => {
+    const currentIndex = checkedCore.indexOf(value);
+    const newChecked = [...checkedCore];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setCheckedCore(newChecked);
+  };
+
+  //AddOn
+  const [checkedAddOn, setCheckedAddOn] = React.useState([0]);
+
+  const handleToggleAddOn = (value) => () => {
+    const currentIndex = checkedAddOn.indexOf(value);
+    const newChecked = [...checkedAddOn];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setCheckedAddOn(newChecked);
+  };
+  //Elective
+  const [checkedElective, setCheckedElective] = React.useState([0]);
+
+  const handleToggleElective = (value) => () => {
+    const currentIndex = checkedElective.indexOf(value);
+    const newChecked = [...checkedElective];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setCheckedElective(newChecked);
   };
 
   return (
@@ -55,6 +110,120 @@ export default function CoursesForm() {
               <MenuItem value={"ECE"}>ECE</MenuItem>
             </Select>
           </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h7" gutterBottom>
+            Core Courses
+          </Typography>
+          <List className={classes.root}>
+            {[0, 1, 2, 3].map((value) => {
+              const labelId = `checkbox-list-label-${value}`;
+
+              return (
+                <ListItem
+                  key={value}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={handleToggleCore(value)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checkedCore.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    id={labelId}
+                    primary={`Core Course ${value + 1}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="comments">
+                      <CommentIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h7" gutterBottom>
+            Addons
+          </Typography>
+          <List className={classes.root}>
+            {[0, 1, 2, 3].map((value) => {
+              const labelId = `checkbox-list-label-${value}`;
+
+              return (
+                <ListItem
+                  key={value}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={handleToggleAddOn(value)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checkedAddOn.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={`Addon ${value + 1}`} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="comments">
+                      <CommentIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h7" gutterBottom>
+            Electives
+          </Typography>
+          <List className={classes.root}>
+            {[0, 1, 2, 3].map((value) => {
+              const labelId = `checkbox-list-label-${value}`;
+
+              return (
+                <ListItem
+                  key={value}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={handleToggleElective(value)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checkedElective.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    id={labelId}
+                    primary={`Elective ${value + 1}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="comments">
+                      <CommentIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
         </Grid>
       </Grid>
     </React.Fragment>
