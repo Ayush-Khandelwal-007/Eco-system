e157f25aea856b3276a45f1037d539999c815757
Ayush <ayushkhandelwal3217@gmail.com>
2021-05-21 03:14:50 +0530

    Add and replace profile pic feature added
    Signed-off-by: Ayush <ayushkhandelwal3217@gmail.com>

--- src/StudentComponents/Sidebar.jsx
+++ src/StudentComponents/Sidebar.jsx
@@ -1,21 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import sidebar from "./SidebarComponents/Sidebar.module.css";
 import garbage from "../images/garbage.svg";
 import { Link, useLocation } from "react-router-dom";
-import { Button } from "@material-ui/core";
 import { useUser } from "../contexts/User";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider } from '@material-ui/core'
+import FileUpload from "./SidebarComponents/FileUpload";
+import { db, storage } from '../Firebase';
+import add from '../images/add.svg'
 
 function Sidebar() {
   const location = useLocation();
   const [state, dispatch] = useUser();
+  const [openAddImageDialog, setOpenAddImageDialog] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [enabled, setEnabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [imageSource, setImageSource] = useState(state.user.imageurl)
+
+  const upload = () => {
+    setLoading(true);
+    const uploadTask = storage.ref(`profileImages/${state.user.roll.toLowerCase()}.jpeg`).put(selectedFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        console.log(`${state.user.roll.toLowerCase()}.jpeg`)
+        storage
+          .ref("profileImages")
+          .child(`${state.user.roll.toLowerCase()}.jpeg`)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("Students").doc(state.user.email).set({
+              imageurl: url
+            }, { merge: true });
+            setLoading(false);
+            setEnabled(false);
+            setSelectedFile(null);
+            setOpenAddImageDialog(false);
+            setImageSource(url);
+          });
+      }
+    );
+  }
+
   return (
     <div className={sidebar.sidebar}>
+      <Dialog open={openAddImageDialog}>
+        <DialogTitle id="form-dialog-title">Select new Profile Pic</DialogTitle>
+        <Divider />
+        <DialogContent>
+          {
+            loading === true ? (<div style={{ width: "60vw" }}>LOADING...</div>) : (
+              <FileUpload setSelectedFile={setSelectedFile} setEnabled={setEnabled} selectedFile={selectedFile} enabled={enabled} />
+            )
+          }
+
+        </DialogContent>
+        <Divider />
+        <DialogActions>
+          <Button disabled={loading} onClick={() => { setOpenAddImageDialog(false) }} color="primary">
+            Cancel
+                    </Button>
+          <Button onClick={() => { upload() }} disabled={(!enabled) || (loading)} color="primary">
+            Upload
+                    </Button>
+        </DialogActions>
+      </Dialog>
       <div className={sidebar.info}>
-        <img
-          src={state?.user?.imageurl}
-          alt="Profile pic"
-          className={sidebar.profilepic}
-        />
+        {
+          imageSource ? (
+            <>
+            <div
+              onClick={() => { setOpenAddImageDialog(true) }}
+              style={{backgroundImage:`url(${imageSource})`}}
+              className={sidebar.profilepic}
+            ><img className={sidebar.addIcon} src={add}/></div>
+            </>
+          ) : (
+            <div onClick={() => { setOpenAddImageDialog(true) }} className={sidebar.profilepic}><img src={add}/>Add Image</div>
+          )
+        }
         <div className={sidebar.enroll}>{state?.user?.roll}</div>
         <div className={sidebar.name}>{state?.user?.name}</div>
         <div className={sidebar.sem}>
@@ -24,10 +97,10 @@ function Sidebar() {
           {state?.user?.semester == 1
             ? "st"
             : state?.user?.semester == 2
-            ? "nd"
-            : state?.user?.semester == 3
-            ? "rd"
-            : "th"}
+              ? "nd"
+              : state?.user?.semester == 3
+                ? "rd"
+                : "th"}
         </div>
       </div>
       <div className={sidebar.links}>
