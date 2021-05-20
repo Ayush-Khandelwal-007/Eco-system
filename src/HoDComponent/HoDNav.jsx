e99766976effb8ed80431862f45880bc2b301127
Ayush <ayushkhandelwal3217@gmail.com>
2021-05-21 01:25:11 +0530

    code clean up
    Signed-off-by: Ayush <ayushkhandelwal3217@gmail.com>

--- src/HoDComponent/HoDNav.jsx
+++ src/HoDComponent/HoDNav.jsx
@@ -226,7 +226,7 @@ function HoDNav() {
         }}
       >
         <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
+          <Badge badgeContent={notifications.length} color="secondary">
             <NotificationsIcon />
           </Badge>
         </IconButton>
@@ -318,11 +318,11 @@ function HoDNav() {
         </div>
         <div className={classes.grow} />
         <div className={classes.sectionDesktop}>
-          <IconButton aria-label="show 4 new mails" color="inherit">
+          {/* <IconButton aria-label="show 4 new mails" color="inherit">
             <Badge badgeContent={4} color="secondary">
               <MailIcon />
             </Badge>
-          </IconButton>
+          </IconButton> */}
           <IconButton
             aria-label={"show new notifications"}
             color="inherit"
